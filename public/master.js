345c83377b5bf503743ad0493e71c4d8212f6eae
Josh Hepworth <josh@friendsoftheweb.com>
2013-07-05 17:47:46 -0400

    Update Style of Gem in a Box
    Introduced a more readable, usable style to the application that
    reduces the amount of information that repeat users need to dismiss
    without removing it so far that a first time user cannot access it.
    
    This involed making the individual gem page accessible for all gems
    by a link, using icons to represent common actions, and removing the setup
    instructions from every page.

--- public/master.js
+++ public/master.js
@@ -1,5 +1,15 @@
 (function($) {
-  $("form.delete-form").submit(function() {
+  $("form.js-delete-form").on("submit", function() {
     return confirm("Are you sure you want to delete this gem?");
+  });
+
+  $setup = $("#setup-instructions");
+  $setup.hide();
+  $(window).on("hashchange", function() {
+    if (window.location.hash.match(/setup/i)) {
+      $setup.slideDown();
+    } else {
+      $setup.slideUp();
+    }
   });
 })(jQuery);
